761a28c169fa28874a5a3b8e0edf105f7ae16442
Sasmita-das <86401220+Sasmita-das@users.noreply.github.com>
2021-06-24 21:16:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import './App.css';
-import SearchBox from './components/SearchBox';
 import Search from './components/Search';
 function App() {
   return (
     <div className="App">
-      {/* <SearchBox /> */}
       <Search />
     </div>
   );
