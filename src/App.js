5b0729f38e772061d2358d93ec2c04a61f1f6530
sasmita-jhili <82135769+sasmita-jhili@users.noreply.github.com>
2021-06-24 20:27:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import SearchBox from './components/SearchBox';
+import Search from './components/Search';
+function App() {
+  return (
+    <div className="App">
+      {/* <SearchBox /> */}
+      <Search />
+    </div>
+  );
+}
+
+export default App;
